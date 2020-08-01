3e15b990fc23100a9317926c9c505809a6f1fcc8
AndrewLevin <amlevin@cern.ch>
2020-08-01 06:02:16 +0200

    update

--- app/src/main/java/com/nplat/MainActivityPostArrayAdapter.java
+++ app/src/main/java/com/nplat/MainActivityPostArrayAdapter.java
@@ -1,14 +1,18 @@
 package com.nplat;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.nplat.ui.main.PageViewModel;
 
+import java.net.URI;
 import java.util.List;
 
 public class MainActivityPostArrayAdapter extends ArrayAdapter<PageViewModel.Post> {
@@ -33,15 +37,19 @@ public class MainActivityPostArrayAdapter extends ArrayAdapter<PageViewModel.Pos
         TextView text_textview;
         TextView username_textview;
         TextView timestamp_textview;
+        ImageView image_imageview;
 
-        post_view = inflater.inflate(R.layout.post, parent, false);
+        post_view = inflater.inflate(R.layout.postwithimage, parent, false);
         text_textview = (TextView) post_view.findViewById(R.id.text);
         username_textview = (TextView) post_view.findViewById(R.id.username);
         timestamp_textview = (TextView) post_view.findViewById(R.id.timestamp);
+        image_imageview = (ImageView) post_view.findViewById(R.id.image);
 
         text_textview.setText(post_list.get(position).text);
         username_textview.setText(post_list.get(position).username);
         timestamp_textview.setText(post_list.get(position).timestamp);
+        Glide.with(this.context).load("https://n-plat.com/image1.jpeg").into(image_imageview);
+//        image_imageview.setImageURI(Uri.parse("https://n-plat.com/image1.jpeg"));
 
         return post_view;
     }
--- app/src/main/java/com/nplat/PostActivityPostArrayAdapter.java
+++ app/src/main/java/com/nplat/PostActivityPostArrayAdapter.java
@@ -1,11 +1,13 @@
 package com.nplat;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import java.util.List;
 
@@ -35,15 +37,19 @@ public class PostActivityPostArrayAdapter extends ArrayAdapter<PostActivity.Post
         TextView post_textview;
         TextView post_usernameview;
         TextView post_timestampview;
+        VideoView post_videoview;
 
-        contact_view = inflater.inflate(R.layout.post, parent, false);
+        contact_view = inflater.inflate(R.layout.postwithvideo, parent, false);
         post_textview = (TextView) contact_view.findViewById(R.id.text);
         post_usernameview = (TextView) contact_view.findViewById(R.id.username);
         post_timestampview = (TextView) contact_view.findViewById(R.id.timestamp);
+        post_videoview = (VideoView) contact_view.findViewById(R.id.video);
 
         post_textview.setText(post_list.get(position).text);
         post_usernameview.setText(post_list.get(position).username);
         post_timestampview.setText(post_list.get(position).timestamp);
+        post_videoview.setVideoURI(Uri.parse("https://n-plat.com/video1.mp4"));
+        post_videoview.start();
 
 
         return contact_view;
